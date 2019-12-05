9aeaf1cae00f8319f4e98f618b869d61a4f05d46
TravelFiend <mikegrace274@gmail.com>
2019-12-04 17:11:10 -0800

    writeJSON function and test done

--- __tests__/fs-functions.test.js
+++ __tests__/fs-functions.test.js
@@ -1,11 +1,34 @@
-const { mkdirp } = require('../lib/fs-functions');
+const fs = require('fs').promises;
+const { mkdirp, writeJSON } = require('../lib/fs-functions');
 
 describe('fs functions', () => {
-    describe('mkdirp', () => {
-        it('should create directory and all parent directories', () => {
-            return mkdirp('./this/is/it')
-            .then()
-            expect('yourMom');
+    // describe('mkdirp', () => {
+    //     it('should create directory and all parent directories', () => {
+    //         return mkdirp('./this/is/it')
+    //         .then()
+    //         expect('yourMom');
+    //     });
+    // });
+    beforeAll(() => {
+        return mkdirp('./it/got');
+    });
+            
+    describe('write JSON', () => {
+        it('should turn an object to JSON and write to a file', () => {
+            return writeJSON('./it/got/thing', {
+                bald: true,
+                eyes: 2
+            })
+                .then(() => {
+                    return fs.readFile('./it/got/thing', 'utf8');
+                })
+                .then(contents => {
+                    expect(JSON.parse(contents)).toEqual(({
+                        bald: true,
+                        eyes: 2
+                    }
+                    ));
+                });
         });
     });
 });
